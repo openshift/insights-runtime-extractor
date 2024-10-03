9118d1432cb5bf487a9a5e2fbaa3cddd038d1b66
Jeff Mesnil <jmesnil@redhat.com>
2024-10-03 15:50:37 +0200

    Update rust-dev container image
    Rewrite the Containerfile-rust-dev so that it can be used for local tests as well
    as testing the code in OpenShift CI
    
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

--- scripts/build-rust-dev.sh
+++ scripts/build-rust-dev.sh
@@ -4,4 +4,4 @@ echo "==========================================================================
 echo "Build multi arch image $IMG"
 echo "=========================================================================="
 
-podman build --platform linux/arm64,linux/amd64 -f Containerfile.rust-dev -t $IMG .
+podman build --platform linux/arm64,linux/amd64 -f Containerfile-rust-dev -t $IMG .
--- scripts/rust-dev.sh
+++ scripts/rust-dev.sh
@@ -4,4 +4,8 @@ if [[ -z "${TARGETARCH}"  ]]; then
 fi
 # Runing a root with privileged to get all the required capabilities
 
-podman run -it --rm -u 0 --privileged -e TARGETARCH=${TARGETARCH}  -v `pwd`/extractor:/home/dev/extractor/  -w /home/dev/extractor/ rust-dev
+podman run -it --rm -u 0 --privileged \
+  -e TARGETARCH=${TARGETARCH} \
+  -v `pwd`:/opt/app-root/src/insights-runtime-extractor \
+  -w /opt/app-root/src/insights-runtime-extractor/extractor \
+  rust-dev /bin/bash
