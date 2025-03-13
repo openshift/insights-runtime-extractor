4aaa21f840f76f822b640cb30207b48a5ffd9652
Libor Fuka <lfuka@lfuka-new-notebook.redhat.com>
2025-03-13 07:45:54 +0100

    SB 3.4.3 test app and 3.1.4 app simplified

--- exporter/test/e2e/spring_boot_test.go
+++ exporter/test/e2e/spring_boot_test.go
@@ -8,7 +8,7 @@ import (
 	"sigs.k8s.io/e2e-framework/pkg/features"
 )
 
-func TestSpringBoot(t *testing.T) {
+func TestSpringBootJar_3_1_4(t *testing.T) {
 
 	appName := "spring-boot"
 	containerName := "main"
@@ -28,6 +28,33 @@ func TestSpringBoot(t *testing.T) {
 				Runtimes: []types.RuntimeComponent{{
 					Name:    "Spring Boot",
 					Version: "3.1.4",
+				}},
+			}
+			g.Expect(runtimeInfo).Should(Ω.Equal(expected))
+		}))
+	_ = testenv.Test(t, feature.Feature())
+}
+
+func TestSpringBootWar_3_4_3(t *testing.T) {
+
+	appName := "spring-boot"
+	containerName := "main"
+	image := "quay.io/insights-runtime-extractor-samples/spring-boot:3.4.3"
+	deployment := newAppDeployment(namespace, appName, 1, containerName, image)
+
+	feature := features.New("Spring Boot from "+image).
+		Setup(deployTestResource(deployment, appName)).
+		Teardown(undeployTestResource(deployment, appName)).
+		Assess("runtime info extracted", checkExtractedRuntimeInfo(namespace, "app="+appName, containerName, func(g *Ω.WithT, runtimeInfo types.ContainerRuntimeInfo) {
+			expected := types.ContainerRuntimeInfo{
+				Os:              "rhel",
+				OsVersion:       "8.10",
+				Kind:            "Java",
+				KindVersion:     "21.0.6",
+				KindImplementer: "Red Hat, Inc.",
+				Runtimes: []types.RuntimeComponent{{
+					Name:    "Spring Boot",
+					Version: "3.4.3",
 				}},
 			}
 			g.Expect(runtimeInfo).Should(Ω.Equal(expected))
