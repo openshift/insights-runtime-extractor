037c6dbbc292d71284f1004537aaa904b019cdb9
Jeff Mesnil <jmesnil@redhat.com>
2025-11-26 09:33:14 +0100

    Add runtime sample for JBoss EAP 8.1
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

---
+++ runtime-samples/jboss-eap/8.1/src/main/java/org/jboss/eap/helloworld/RestApplication.java
@@ -0,0 +1,9 @@
+package org.jboss.eap.helloworld;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/services")
+public class RestApplication extends Application {
+
+}
